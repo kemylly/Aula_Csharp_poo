f8f61d5be874c09fc634e9c9f2ddef0ceeb01d13
kemylly <kemylly@users.noreply.github.com>
2020-09-06 23:19:44 -0300

    Comecei o projeto

---
+++ Mensagem_poo/Mensagem_poo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mensagem_poo
+{
+    class Program //classe
+    {
+        static void Main(string[] args) //metoodo que chama main e executa uma acao
+        {
+            Console.WriteLine("Alo mundo");
+            Console.ReadKey();
+        }
+    }
+}
