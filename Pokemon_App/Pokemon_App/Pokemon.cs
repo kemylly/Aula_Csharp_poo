767c6fef33ec815378b26715321ad4c598b69d8f
kemylly <kemylly@users.noreply.github.com>
2020-09-07 20:33:37 -0300

    Criei pokemon app e criei a classe

---
+++ Pokemon_App/Pokemon_App/Pokemon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_App
+{
+    public class Pokemon
+    {
+		private String nome;  //propriedade
+
+		public String Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+		private String dadosPokemon;
+
+		public String DadosPokemon
+		{
+			get { return dadosPokemon; }
+			set { dadosPokemon = value; }
+		}
+
+
+	}
+}
