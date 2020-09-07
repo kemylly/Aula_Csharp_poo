0b1d919febc69428c1d580dcbaace20bf2aa0400
kemylly <kemylly@users.noreply.github.com>
2020-09-07 00:08:48 -0300

    Adicionei Mensagem e algumas classes

---
+++ Mensagem_poo/Mensagem_poo/Mensagem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mensagem_poo
+{
+    public class Mensagem
+    {
+        //String TextoMensagem; //normalmente uma variavel representa um objeto / caracteristicas
+        //naturamente ela vem encapsulada / escondida
+
+        //public String TextoMensagem; //publico
+        private String TextoMensagem;
+
+        //private e public
+        public void ExibirMensagem() //void nao retorna nada
+        {
+            Console.WriteLine(this.TextoMensagem); //this é interagindo com ele mesmo
+        }
+
+        public String getTextoMensagem()
+        {
+            return this.TextoMensagem;
+        }
+
+        public void setTextoMensagem(String valor)
+        {
+            this.TextoMensagem = valor.ToUpper(); // deiixar todos maisculo
+        }
+    }
+}
--- Mensagem_poo/Mensagem_poo/Program.cs
+++ Mensagem_poo/Mensagem_poo/Program.cs
@@ -8,9 +8,24 @@ namespace Mensagem_poo
 {
     class Program //classe
     {
-        static void Main(string[] args) //metoodo que chama main e executa uma acao
+        static void Main(string[] args) //objeto metodo que chama main e executa uma acao
         {
-            Console.WriteLine("Alo mundo");
+            //Console.WriteLine("Alo mundo");
+            
+            Mensagem msg1, msg2;
+            
+            msg1 = new Mensagem(); //construtor / criou o objeto / deu vida
+            //msg1.TextoMensagem = "Alo mundo"; //metodos
+            msg1.setTextoMensagem("Alo mundo");
+            Console.WriteLine(msg1.getTextoMensagem());
+
+            msg1.ExibirMensagem();
+            //Console.WriteLine(msg1.TextoMensagem);
+
+            //msg2 = new Mensagem();
+            //msg2.TextoMensagem = "Segundo objeto";
+            //msg2.ExibirMensagem();
+
             Console.ReadKey();
         }
     }
