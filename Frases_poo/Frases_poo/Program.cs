8f652fb3a244f6ff410bb2f2718f937e45fe892a
kemylly <kemylly@users.noreply.github.com>
2020-09-13 08:29:52 -0300

    Criei o projeto frases poo

---
+++ Frases_poo/Frases_poo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frases_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
